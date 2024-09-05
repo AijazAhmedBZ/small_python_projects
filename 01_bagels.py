a9ddcd1420e1fd9452863b7ef2264925b84ace6e
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2024-09-05 09:01:02 +0500

    updated on 05-Sep-24

--- 01_bagels.py
+++ 01_bagels.py
@@ -15,11 +15,84 @@ def main():
     
     
     
-    print("""I am thinking of a three digit number. Try to guess what it is
-            Here are some clues:
+    print(f"""I am thinking of a {} digit number. Try to guess what it is
+            Try to guess what it is. Here are some clues:
             When I say:     That means:
             Pico            One digit is correct but in the wrong position.
             Fermi           One digit is correct and in the right position.
             Bagels          No digit is correct.
-            I have thought up a number.
-            You have 10 guesses to get it.""").__format__(max_guesses)
+          
+            For example, if the secret number was 248 and your guess was 843, the
+            clue would be Fermi Pico.""").format(num_digits)
+    while True:
+
+
+        secret_num = get_secret_num()
+        print("I have thought up a number.")
+        print("You have {} guesses to get it.".format(max_guesses))
+
+        num_guesses = 1
+        while num_guesses <= max_guesses:
+            guess = ""
+        
+            while len(guess) != num_digits or not guess.isdecimal():
+                print("Guess #{}: ".format(num_guesses))
+                guess = input("> ")
+
+            clues = get_clues(guess, secret_num)
+            print(clues)
+            num_guesses += 1
+
+            if guess == secret_num:
+                break
+            if num_guesses > max_guesses:
+                print("You ran out of guesses.")
+                print("The answer was {}.".format(secret_num))
+
+        
+        print("Do you want to play again? (yes or no)")
+        if not input("> ").lower().startswith("y"):
+            break
+    print("Thanks for playing!")
+
+
+def get_secret_num():
+
+    numbers = list("0123456789")
+    random.shuffle(numbers)
+
+
+    secret_num = ""
+    for i in range(num_digits):
+        secret_num += str(numbers[i])
+    return secret_num
+
+
+def get_clues(guess, secret_num):
+
+
+    if guess == secret_num:
+        return "You got it!"
+
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secret_num[i]:
+
+            clues.append("Fermi")
+        elif guess[i] in secret_num:
+
+            clues.append("Pico")
+    if len(clues) == 0:
+        return "Bagels"
+    else:
+
+
+        clues.sort()
+
+        return " ".join(clues)
+        
+
+
+if __name__ == "__main__":
+    main()
